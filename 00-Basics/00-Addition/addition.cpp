d90348ce4d22fb5ea76afbe2b93c6784c935f715
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-19 16:15:14 +0100

    Create addition.cpp

---
+++ 00-Basics/00-Addition/addition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x, y, sum;
+    
+    cout << "Enter the two numbers : ";
+    cin >> x >> y;
+
+    sum = x + y;
+ 
+    cout << x << " + " <<  y << " = " << sum;     
+    return 0;
+}
