a678ae1037d2b9e204317f508e993b3557845819
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-19 16:31:42 +0100

    Create getbigger.cpp

---
+++ 00-Basics/02-GetBigger/getbigger.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x, y, max;
+    
+    cout << "Enter the two numbers : ";
+    cin >> x >> y;
+ if (x>y){
+ 	 cout << x;
+ }
+ else {
+ 	 cout << y;
+ }
+  return 0;
+}
