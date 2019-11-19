660b3865c36df2a2abc0dd988d4dc066d78c9b8e
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-19 16:25:52 +0100

    Create isEqual.cpp

---
+++ 00-Basics/01-IsEqual/isEqual.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x, y, sum;
+    
+    cout << "Enter the two numbers : ";
+    cin >> x >> y;
+
+ if (x==y){
+ 	cout<<"equal";}
+ 	else {
+ 		cout<<"not equal";
+	 }
+   
+    return 0;
+}
