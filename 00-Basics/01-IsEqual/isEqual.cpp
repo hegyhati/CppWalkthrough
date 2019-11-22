031e838f13a6c8f3d4eb4c580fbe154ab12cc5cc
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-22 17:34:49 +0100

    Update isEqual.cpp

--- 00-Basics/01-IsEqual/isEqual.cpp
+++ 00-Basics/01-IsEqual/isEqual.cpp
@@ -4,16 +4,19 @@ using namespace std;
 
 int main()
 {
-    int x, y, sum;
+    int x, y;
     
     cout << "Enter the two numbers : ";
     cin >> x >> y;
 
- if (x==y){
- 	cout<<"equal";}
- 	else {
- 		cout<<"not equal";
-	 }
+     if (x==y)
+     {
+ 	    cout<<"equal";
+     }
+     else 
+     {
+     	cout<<"not equal";
+     }
    
-    return 0;
+     return 0;
 }
