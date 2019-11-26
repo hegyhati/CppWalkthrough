1be4b5e4f14f462bffa06be3279c59a278be4b64
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-26 08:56:42 +0100

    Create count.cpp

---
+++ 00-Basics/06-CountPositivesUntilZero/count.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+    
+int n ;
+int sum = 0;
+
+     cout << "Enter your numbers   : " << endl;
+
+while(1)
+{
+
+     cin >> n;
+
+if(n == 0) break;
+
+else if (n>0)
+     {
+   	 sum++;
+     }
+}
+     cout << sum << endl;
+return 0;
+   
+          }
+
+
