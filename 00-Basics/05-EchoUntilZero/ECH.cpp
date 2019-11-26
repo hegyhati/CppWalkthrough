62b5042ff73cc16fa5c601aa4addbaf9f1ad310a
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-26 08:44:33 +0100

    without array

--- 00-Basics/05-EchoUntilZero/ECH.cpp
+++ 00-Basics/05-EchoUntilZero/ECH.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main(){
 	
-    int  arr [8], i;
     
-    cout<< "Please Enter Your Numbers "<<endl;
-    
-    for( i = 0; i<8; i++)
-	{
-        cin >> arr[i];
-    }
-    
-    cout <<"Your Numbers Before Zero Are :  "<<endl;
-    
-    for(i = 0; i<8; i++)
-	{
-        if (arr[i]== 0) break;
-        else cout<<arr[i]<<endl;  
-    }
-    return 0;
+int n;
+
+cout << "Enter your numbers to check if it is zero  : " << endl;
+
+while(1){
+
+cin >> n;
+
+if(n == 0) break;
+
+else cout << n << endl;
+
+}
+
+return 0;
+
 }
 
 
