b1e24c9c1bb15585d84da4a761f13361bdad584c
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-11-26 09:10:02 +0100

    Create max.cpp

---
+++ 00-Basics/07-FindMaximalUntilZero/max.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+    
+int n ,x;
+int max ;
+
+     cout << "Enter your numbers   : " << endl;
+     cin >> x;
+     max=x;
+while(1)
+{
+
+     cin >> n;
+     
+if(n == 0) break;
+
+else if (max<n)
+     {
+   	 max=n;
+     }
+}
+     cout << max << endl;
+return 0;
+   
+          }
+
+
