053c2a7d6fa2908d4fa200e27fb4c6ef0ce4d22c
chemmakh <43638930+chwcm@users.noreply.github.com>
2019-12-17 00:53:12 +0100

    Create Digits.cpp

---
+++ 00-Basics/15-Digits/Digits.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std; 
+  
+
+int main() 
+{ 
+int n;
+cin>>n;
+int count = 0; 
+int sum=0;
+    while (n != 0) { 
+        sum = sum + n % 10; 
+        n = n / 10; 
+        count++; 
+	
+    }
+
+  cout << count<<endl;
+  cout<<sum;
+} 
