0961c2cc3d998305a99d7fd34cb573cf80aac8c5
chemmakh <43638930+chwcm@users.noreply.github.com>
2020-02-06 05:10:49 +0100

    Create setoperations.cpp

---
+++ 01-ArrayManipulation/05-SetOperations/setoperations.cpp
@@ -0,0 +1,91 @@
+
+#include<iostream>
+  
+using namespace std; 
+int unionarr (int a[], int b[], int n, int m) 
+{
+int i = 0, j = 0; 	
+	
+	 while (i < n && j < m)  
+    { 
+                  
+        if (a[i] < b[j])  
+        { 
+          cout << a[i++]; 
+        }  
+                  
+        else 
+        if (b[j] < a[i])  
+        { 
+           cout << b[j++]; 
+        }  
+        else 
+        { 
+             
+            cout << b[j++] ; 
+            i++; 
+             
+        } 
+    } 
+    while (i<n)
+	 cout << a[i++]; 
+	while (j<m)
+	 cout << b[j++]; 
+
+} 
+
+void intersection(int a[], int b[], int n, int m) 
+{ 
+    int i = 0, j = 0; 
+      
+    while (i < n && j < m)  
+    { 
+                  
+        if (a[i] > b[j])  
+        { 
+            j++; 
+        }  
+                  
+        else 
+        if (b[j] > a[i])  
+        { 
+            i++; 
+        }  
+        else 
+        { 
+             
+            cout << a[i] << " "; 
+            i++; 
+            j++; 
+        } 
+    } 
+} 
+  
+
+int main() 
+{ 
+
+
+int n = 5;
+int m = 4;
+    int a[m] ; 
+    int b[n] ;
+    int i , j ;
+    	cout << "enter the elements of  a : "<<endl;
+    for (i = 0; i < m; i++) {
+    
+    	cin>>a[i];
+    
+	}
+
+    	cout << " enter the elements of  b : "<<endl;
+	for (j = 0; j < n; j++) {
+			
+    	cin>>b[j];
+    
+	}
+    cout<<" union : ";
+    unionarr(a, b, m, n);
+     cout<<" inter : ";
+    intersection(a, b, m, n); 
+} 
