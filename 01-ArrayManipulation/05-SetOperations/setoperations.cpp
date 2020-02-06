414747a4e9ead2f57de85a3254b184fa882f1c93
chemmakh <43638930+chwcm@users.noreply.github.com>
2020-02-06 21:05:39 +0100

    Update setoperations.cpp

--- 01-ArrayManipulation/05-SetOperations/setoperations.cpp
+++ 01-ArrayManipulation/05-SetOperations/setoperations.cpp
@@ -2,6 +2,76 @@
 #include<iostream>
   
 using namespace std; 
+
+
+
+int Differarr1 (int a[], int b[], int n, int m) 
+{
+
+int i = 0, j = 0; 	
+
+	
+	 while (i < n && j < m)  
+    { 
+                  
+        if (a[i] < b[j])  
+        { 
+          cout << a[i]<<" "; 
+          i++;
+        }  
+                  
+        else 
+        if (b[j] < a[i])  
+        { 
+           cout << b[j]<<" "; 
+           j++;
+        }  
+        else 
+        { 
+           i++;
+           j++;
+		}
+	}
+
+	
+
+}
+int Differarr2 (int a[], int b[], int n, int m) 
+{
+
+int i = 0, j = 0; 	
+
+	
+	 while (i < n && j < m)  
+    { 
+                  
+        if (b[j] < a[i])  
+        { 
+         cout << b[j]<<" "; 
+         j++;
+        }  
+                  
+        else 
+        if (b[j] > a[i])  
+        { 
+          i++;
+        }  
+        else 
+        { 
+           i++;
+           j++;
+		}
+	}
+
+	while (i<n)
+	 cout << a[i++]<<" ";  
+	while (j<m)
+	 cout << b[j++]<<" ";  
+
+
+}
+
+
 int unionarr (int a[], int b[], int n, int m) 
 {
 int i = 0, j = 0; 	
@@ -11,30 +81,30 @@ int i = 0, j = 0;
                   
         if (a[i] < b[j])  
         { 
-          cout << a[i++]; 
+          cout << a[i++]<<" ";  
         }  
                   
         else 
         if (b[j] < a[i])  
         { 
-           cout << b[j++]; 
+           cout << b[j++]<<" ";  
         }  
         else 
         { 
              
-            cout << b[j++] ; 
+            cout << b[j++] <<" ";  
             i++; 
              
         } 
     } 
     while (i<n)
-	 cout << a[i++]; 
+	 cout << a[i++]<<" ";  
 	while (j<m)
-	 cout << b[j++]; 
+	 cout << b[j++]<<" ";  
 
 } 
 
-void intersection(int a[], int b[], int n, int m) 
+int intersection(int a[], int b[], int n, int m) 
 { 
     int i = 0, j = 0; 
       
@@ -71,21 +141,27 @@ int m = 4;
     int a[m] ; 
     int b[n] ;
     int i , j ;
-    	cout << "enter the elements of  a : "<<endl;
+    //	cout << "enter the elements of  a : "<<endl;
     for (i = 0; i < m; i++) {
     
     	cin>>a[i];
     
 	}
 
-    	cout << " enter the elements of  b : "<<endl;
+    //	cout << " enter the elements of  b : "<<endl;
 	for (j = 0; j < n; j++) {
 			
     	cin>>b[j];
     
 	}
-    cout<<" union : ";
-    unionarr(a, b, m, n);
-     cout<<" inter : ";
-    intersection(a, b, m, n); 
+  unionarr(a, b, m, n) ;
+  cout<<"   "<<endl;   
+  intersection(a, b, m, n);
+  cout<<"   "<<endl; 
+  Differarr1(a, b, m, n);
+  cout<<"   "<<endl; 
+  Differarr2(a, b, m, n);
+  
+  return 0;
+    
 } 
